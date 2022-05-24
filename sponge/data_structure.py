f25b1827ece0f4bcbfe93042b16aa5310f95d859
Eladio Sareno Jr <eladio.sareno@netzontech.com>
2022-05-24 19:49:48 +0800

    chore(data-structure): implement data structure

--- sponge/data_structure.py
+++ sponge/data_structure.py
@@ -1,28 +1,21 @@
-from typing import List, Optional
+from typing import List
 
 from pydantic.fields import Field
 from pydantic.main import BaseModel
 
 
 class Transaction(BaseModel):
-    # payload
     timestamp: int = Field(...)
-    txn_from: str = Field(...)
-    txn_to: str = Field(...)
-    amount: float = Field(...)
-    metadata: Optional[str] = Field(default=None)
-    # header
-    hash: Optional[str] = Field(default=None)
-    block_signature: str = Field(...)
+    payload: str = Field(...)
+    hash: str = Field(...)
+    signature: str = Field(...)
     public_key: str = Field(...)
 
 
 class Block(BaseModel):
-    # payload
-    timestamp: int = Field(...)
     previous_hash: str = Field(...)
     nonce: int = Field(...)
+    timestamp: int = Field(...)
     transactions: List[Transaction]
-    # header
-    miner_address: Optional[float] = Field(default=None)
-    hash: Optional[str] = Field(default=None)
+    merkle_root: str = Field(...)
+    miner_address: str = Field(...)
